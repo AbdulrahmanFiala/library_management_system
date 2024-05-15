67b3fee470df9717fdaf38d5ffcfeb3eacee3744
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-05-16 01:10:41 +0300

    fix the import statement

--- src/main/java/com/fiala/library_management_system/dao/RecordRepository.java
+++ src/main/java/com/fiala/library_management_system/dao/RecordRepository.java
@@ -1,5 +1,6 @@
 package com.fiala.library_management_system.dao;
 
+import com.fiala.library_management_system.entity.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
