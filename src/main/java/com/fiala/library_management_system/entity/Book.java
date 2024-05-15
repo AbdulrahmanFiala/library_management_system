84a363fb86be76f11f1bbab03eb4935eea8ea227
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-05-15 17:54:26 +0300

    Create Book.java

---
+++ src/main/java/com/fiala/library_management_system/entity/Book.java
@@ -0,0 +1,29 @@
+package com.fiala.library_management_system.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.Year;
+
+@Entity
+@Table(name ="book")
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "publication_year")
+    private Year publicationYear;
+
+    @Column(name = "isbn")
+    private  String isbn;
+
+}
