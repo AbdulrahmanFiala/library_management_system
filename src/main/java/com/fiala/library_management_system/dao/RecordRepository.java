ab6e9243932ccd4399239fb66f30558236db2250
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-05-18 02:12:45 +0300

    Add abstract method
    Add abstract method to get record by patronId and bookId

--- src/main/java/com/fiala/library_management_system/dao/RecordRepository.java
+++ src/main/java/com/fiala/library_management_system/dao/RecordRepository.java
@@ -3,5 +3,9 @@ package com.fiala.library_management_system.dao;
 import com.fiala.library_management_system.entity.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RecordRepository extends JpaRepository<Record, Long> {
+    Optional<Record> findByPatronIdAndBookId(Long patronId, Long bookId);
+
 }
