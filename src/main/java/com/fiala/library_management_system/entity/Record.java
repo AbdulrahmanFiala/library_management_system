c966222fb8845854697e6f1e0ee90df2af8036d9
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-05-18 02:14:06 +0300

    Update Record.java
    - Add Record constructor.
    - Removing the @Future, since the return date will not be set during the creating of the record.

--- src/main/java/com/fiala/library_management_system/entity/Record.java
+++ src/main/java/com/fiala/library_management_system/entity/Record.java
@@ -9,28 +9,34 @@ import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
 @Entity
-@Table(name ="record")
+@Table(name = "record")
 @Data
 public class Record {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(name = "id")
-        private Long id;
 
-        @NotNull(message = "Borrow date must not be null")
-        @Past(message = "Borrow date must be in the past")
-        @Column(name = "borrow_date")
-        private LocalDate borrowDate;
+    public Record(LocalDate borrowDate, Book book, Patron patron) {
+        this.borrowDate = borrowDate;
+        this.book = book;
+        this.patron = patron;
+    }
 
-        @Future(message = "Return date must be in the future")
-        @Column(name = "return_date")
-        private LocalDate returnDate;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
-        @ManyToOne
-        @JoinColumn(name = "book_id", nullable = false)
-        private Book book;
+    @NotNull(message = "Borrow date must not be null")
+    @Past(message = "Borrow date must be in the past")
+    @Column(name = "borrow_date")
+    private LocalDate borrowDate;
 
-        @ManyToOne
-        @JoinColumn(name = "patron_id", nullable = false)
-        private Patron patron;
+    @Column(name = "return_date")
+    private LocalDate returnDate;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "patron_id", nullable = false)
+    private Patron patron;
 }
