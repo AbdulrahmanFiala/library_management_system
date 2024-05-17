1fc2529e5abaa948e2ffc1b87a496a4af68b40a5
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-05-18 00:02:34 +0300

    Update Book.java
    Add size validation and blank validation to comply with database constraints.

--- src/main/java/com/fiala/library_management_system/entity/Book.java
+++ src/main/java/com/fiala/library_management_system/entity/Book.java
@@ -3,6 +3,9 @@ package com.fiala.library_management_system.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import java.time.Year;
 
 @Entity
@@ -14,16 +17,19 @@ public class Book {
     @Column(name = "id")
     private Long id;
 
+    @Size(max = 45, message = "Name cannot exceed 45 characters")
     @Column(name = "title")
     private String title;
 
+    @Size(max = 45, message = "Author name cannot exceed 45 characters")
     @Column(name = "author")
     private String author;
 
     @Column(name = "publication_year")
     private Year publicationYear;
 
+    @NotBlank(message = "ISBN is mandatory")
+    @Size(max = 13, message = "ISBN is a 13-digit number")
     @Column(name = "isbn")
     private  String isbn;
-
 }
