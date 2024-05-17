75b405da1a235354c90cb0fc5ab60b20cd50a985
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-05-18 00:02:52 +0300

    Update Patron.java
    Add size validation, pattern validation, and blank validation to comply with database constraints.

--- src/main/java/com/fiala/library_management_system/entity/Patron.java
+++ src/main/java/com/fiala/library_management_system/entity/Patron.java
@@ -3,6 +3,10 @@ package com.fiala.library_management_system.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.time.Year;
 
 
@@ -16,12 +20,19 @@ public class Patron {
     @Column(name = "id")
     private Long id;
 
+    @Size(max = 100, message = "Name cannot exceed 100 characters")
+    @NotBlank(message = "Patron name is mandatory")
     @Column(name = "name")
     private String name;
 
+    @Size(max = 255, message = "Email cannot exceed 255 characters")
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Patron email is mandatory")
     @Column(name = "email")
     private String email;
 
+    @Size(max = 20, message = "Phone number can't exceed 20 characters")
+    @Pattern(regexp = "\\+?[0-9]+", message = "Invalid phone number format")
     @Column(name = "phone_number")
     private String phoneNumber;
 }
