8ea671af9923331bf20c7dfcf9851b78600e8d24
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-06-11 05:03:10 +0300

    Create AuthenticationService.java

---
+++ src/main/java/com/fiala/library_management_system/auth/AuthenticationService.java
@@ -0,0 +1,57 @@
+package com.fiala.library_management_system.auth;
+
+import com.fiala.library_management_system.service.JwtService;
+import com.fiala.library_management_system.dto.requests.AuthenticationRequest;
+import com.fiala.library_management_system.dto.responses.AuthenticationResponse;
+import com.fiala.library_management_system.dto.requests.RegisterRequest;
+import com.fiala.library_management_system.entity.User;
+import com.fiala.library_management_system.dao.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstname())
+                .lastName(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(UserRole.USER)
+                .build();
+
+        repository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
