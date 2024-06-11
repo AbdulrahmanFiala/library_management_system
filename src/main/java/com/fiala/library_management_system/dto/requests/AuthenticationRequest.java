96ba9e862982c9396490e64ae54d72d886ada1ce
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-06-11 05:03:29 +0300

    Create AuthenticationRequest.java

---
+++ src/main/java/com/fiala/library_management_system/dto/requests/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.fiala.library_management_system.dto.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    private String email;
+    private String password;
+}
