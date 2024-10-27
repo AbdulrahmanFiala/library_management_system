6397937903e43da2dfc0aad013e4bfd9f1faeb84
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-10-27 17:23:01 +0300

    [CHORE]: Remove redundant imports

--- src/main/java/com/fiala/library_management_system/entity/Record.java
+++ src/main/java/com/fiala/library_management_system/entity/Record.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import java.time.LocalDate;
