db17e95820e69c188981c73d140bfe2ae2badc37
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2025-06-25 04:02:54 +0300

    chore: clean up redundant comments

--- src/test/java/com/fiala/library_management_system/controller/RecordControllerTest.java
+++ src/test/java/com/fiala/library_management_system/controller/RecordControllerTest.java
@@ -24,65 +24,52 @@ class RecordControllerTest {
 
     @Test
     void testBorrowBook_Success() throws Exception {
-        // Mocking
         Long bookId = 1L;
         Long patronId = 2L;
-        when(recordService.borrowBook(bookId, patronId)).thenReturn(null); // Mock successful borrow
+        when(recordService.borrowBook(bookId, patronId)).thenReturn(null);
 
-        // Call the controller method
         ResponseEntity<String> response = recordController.borrowBook(bookId, patronId);
 
-        // Verify
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals("Book borrowed successfully", response.getBody());
     }
 
     @Test
     void testBorrowBook_Failure() throws Exception {
-        // Mocking
         Long bookId = 1L;
         Long patronId = 2L;
         String errorMessage = "Book not available";
         when(recordService.borrowBook(bookId, patronId)).thenThrow(new Exception(errorMessage));
 
-        // Call the controller method
         ResponseEntity<String> response = recordController.borrowBook(bookId, patronId);
 
-        // Verify
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
         assertEquals(errorMessage, response.getBody());
     }
 
     @Test
     void testReturnBook_Success() throws Exception {
-        // Mocking
         Long bookId = 1L;
         Long patronId = 2L;
-        // Mock successful return using doNothing() for void methods
+
         doNothing().when(recordService).returnBook(bookId, patronId);
 
-        // Call the controller method
         ResponseEntity<String> response = recordController.returnBook(bookId, patronId);
 
-        // Verify
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals("Book returned successfully", response.getBody());
     }
 
     @Test
     void testReturnBook_Failure() throws Exception {
-        // Mocking
         Long bookId = 1L;
         Long patronId = 2L;
         String errorMessage = "Book not returned";
 
-        // Use doThrow for methods returning void
         doThrow(new Exception(errorMessage)).when(recordService).returnBook(bookId, patronId);
 
-        // Call the controller method
         ResponseEntity<String> response = recordController.returnBook(bookId, patronId);
 
-        // Verify
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
         assertEquals(errorMessage, response.getBody());
     }
